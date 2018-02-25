087e38a5283c4502ce8435a39fb80299a39e8ff8
mrmx <mistermx@gmail.com>
2018-02-26 00:23:18 +0100

    Added copyright and licens header

--- wuabit-dust-sha256/wuabit-dust-sha256.ino
+++ wuabit-dust-sha256/wuabit-dust-sha256.ino
@@ -1,3 +1,22 @@
+//
+// Wuabit Dust vSHA-256 is a 256bits password/key usb generator.
+//
+// Copyright 2018 by Manuel Polo (https://about.me/mrmx)
+//
+// This file is part of Wuabit Dust vSHA-256 a 256bits password/key usb generator.
+// Wuabit Dust vSHA-256  is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Wuabit Dust vSHA-256 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Wuabit Dust vSHA-256.  If not, see <http://www.gnu.org/licenses/>.
+//
 #include "DigiKeyboard.h"
 #include "usha256.h"
 #include <Entropy.h>
